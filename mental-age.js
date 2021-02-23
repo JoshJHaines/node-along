82a6f7d9499ffd72e6e6afd3d61b8e46dd31f0d6
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-23 14:43:30 -0500

    added ageNextYear function

--- mental-age.js
+++ mental-age.js
@@ -2,11 +2,15 @@ const getInput = require("./get-input.js");
 
 const userAge = getInput();
 const mentalAge = userAge / 2 + 3;
+const ageNextYear = parseInt(userAge) + 1
 
 if (userAge === "--help"){
     console.log("Enter your actual age to find out your mental age.")
 } else {
+    console.log("----------------------------------------------------")
     console.log("You are " + userAge + " years old. So your mental age is " + mentalAge + "!");
+    console.log("You will be " + ageNextYear + " next year!")
+    console.log("----------------------------------------------------")
 }
 
 
